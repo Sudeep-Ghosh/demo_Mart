2afdb4ad3532cef7fcb1d8364ebed96c17be0d82
DeepaliKosta <deepalikosta1234@gmail.com>
2022-10-04 17:35:26 +0530

    Updated Customer entity

--- src/main/java/com/onlinesweetmart/entity/Customer.java
+++ src/main/java/com/onlinesweetmart/entity/Customer.java
@@ -1,15 +1,19 @@
 package com.onlinesweetmart.entity;
 
+import java.util.List;
 import java.util.Set;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
+import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 
 import lombok.AllArgsConstructor;
@@ -24,16 +28,21 @@ import lombok.NoArgsConstructor;
 public class Customer {
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
+	@SequenceGenerator(
+			name = "customer_sequence",
+			sequenceName = "customer_sequence",
+			allocationSize = 1
+			)
+	@GeneratedValue(
+			strategy = GenerationType.SEQUENCE,
+			generator = "customer_sequence"
+			)
 	private Long userId;
 
 	@Column(name = "UserName")
 	private String userName;
 
-	@OneToMany(cascade = CascadeType.ALL)
-	private Set<SweetOrder> sweetOrders;
 
-	@OneToOne(cascade = CascadeType.ALL)
+	@OneToOne(cascade = CascadeType.MERGE,fetch = FetchType.EAGER)
 	private Cart cart;
-
 }
